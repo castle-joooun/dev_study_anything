8ab834d1cf06a86f42c8ec2e48d55b659d28e6af
castle-joooun <tjdwns0309@gmail.com>
2022-09-04 02:26:34 +0900

    기능추가: [FASTapi] API-Read
    - all user
    - one user & filter

--- fastapi-1/router/user.py
+++ fastapi-1/router/user.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm.session import Session
+from typing import List
 
 from db import db_user
 from db.database import get_db
@@ -13,7 +14,18 @@ router = APIRouter()
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
 
-# Read user
+
+# Read all users
+@router.get('/all', response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+    return db_user.get_all_users(db)
+
+
+# Read one user
+@router.get('/{id}', response_model=UserDisplay)
+def get_user_filter(id: int, db: Session = Depends(get_db)):
+    return db_user.get_user(db, id)
+
 
 # Update user
 
