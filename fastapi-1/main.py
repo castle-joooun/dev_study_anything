4cffcf1bc01324f26cbafcadb8b389c00767f649
castle-joooun <tjdwns0309@gmail.com>
2022-09-05 02:57:40 +0900

    기능추가: [FASTapi] DB relationship
    - Add articles table
    - Add article, db_article, model, schema
    - Add article API-Read/Post

--- fastapi-1/main.py
+++ fastapi-1/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, status, Response
 
-from router import blog_get, blog_post, user
+from router import blog_get, blog_post, user, article
 from db import models
 from db.database import engine
 
@@ -8,6 +8,7 @@ app = FastAPI()
 app.include_router(blog_get.router, prefix='/blog', tags=['blog'])
 app.include_router(blog_post.router, prefix='/blog', tags=['blog'])
 app.include_router(user.router, prefix='/user', tags=['user'])
+app.include_router(article.router, prefix='/article', tags=['article'])
 
 
 @app.get('/')
