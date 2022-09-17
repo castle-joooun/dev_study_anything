3678e8b7c303a374ddc8581e987a05cbf695e60d
castle-joooun <tjdwns0309@gmail.com>
2022-09-18 01:28:16 +0900

    기능추가: [fastapi-instagram] post 추가
    - Add post

---
+++ fastapi-ex-instagram/routers/post.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm.session import Session
+
+from db import db_post
+from db.database import get_db
+from db.db_post import PostBase
+from routers.schemas import PostDisplay
+
+
+router = APIRouter(
+    prefix='/post',
+    tags=['post']
+)
+
+image_url_types = ['absolute', 'relative']
+
+
+@router.post('', response_model=PostDisplay)
+def create(request: PostBase, db: Session = Depends(get_db)):
+    if request.image_url_type not in image_url_types:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail='Parameter image_url_type can only take '
+                                   'values "absolute" or "relative')
+
+    return db_post.create(db, request)
+
