e9772f27f01819b488a7fb7e8cbce0a65b4eca1f
castle-joooun <tjdwns0309@gmail.com>
2022-09-25 03:33:12 +0900

    기능추가: [fastapi-warehouse: connect redis] product API-CRD

--- fastapi-ex-warehoust(connect_redis)/main.py
+++ fastapi-ex-warehoust(connect_redis)/main.py
@@ -31,3 +31,31 @@ class Product(HashModel):
 @app.post('/product')
 def create(product: Product):
     return product.save()
+
+
+@app.get('/product/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+
+@app.get('/products')
+def all():
+    # return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
+
+
+def format(pk: str):
+    product = Product.get(pk)
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+
+@app.delete('/product/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
+
+
